f58726af6af98db5978c1dda76ef2be63e6e2710
IvanLuLyf <835498692@qq.com>
2020-04-08 00:37:05 +0800

    更新首页

--- src/main/java/cn/ivanlu/market/controller/IndexController.java
+++ src/main/java/cn/ivanlu/market/controller/IndexController.java
@@ -1,22 +1,16 @@
 package cn.ivanlu.market.controller;
 
-import cn.ivanlu.market.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-    private UserService userService;
-
-    public IndexController(UserService userService) {
-        this.userService = userService;
+    public IndexController() {
     }
 
     @RequestMapping(value = {"/", "/index"})
-    @ResponseBody
     public String index() {
-        return userService.getById(1).toString();
+        return "index";
     }
 }
