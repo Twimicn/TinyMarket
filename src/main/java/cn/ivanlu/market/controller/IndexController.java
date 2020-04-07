e832e19fe99445fd60af5df05671c2f2acb10fd3
IvanLuLyf <835498692@qq.com>
2020-04-07 20:35:56 +0800

    添加动态环境

--- src/main/java/cn/ivanlu/market/controller/IndexController.java
+++ src/main/java/cn/ivanlu/market/controller/IndexController.java
@@ -1,13 +1,18 @@
 package cn.ivanlu.market.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
     @RequestMapping(value = {"/", "/index"})
-    @ResponseBody public String index() {
-        return "Hello World";
+    @ResponseBody
+    public String index() {
+        return dbUrl;
     }
 }
