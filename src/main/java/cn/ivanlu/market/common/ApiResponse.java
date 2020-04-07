e60acd21456277a0e8ec1fadae0379191496a5c4
IvanLuLyf <835498692@qq.com>
2020-04-08 00:37:32 +0800

    添加通用API返回对象类

---
+++ src/main/java/cn/ivanlu/market/common/ApiResponse.java
@@ -0,0 +1,14 @@
+package cn.ivanlu.market.common;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Builder;
+import lombok.Data;
+
+@JsonSerialize
+@Data
+@Builder
+public class ApiResponse<T> {
+    private int status;
+    private String msg;
+    private T data;
+}
